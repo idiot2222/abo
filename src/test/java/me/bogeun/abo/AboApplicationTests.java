93017de58c619839980df5e64a01677c3e547e16
idiot2222 <pky2892@gmail.com>
2021-01-29 21:47:36 +0900

    Initial commit

---
+++ src/test/java/me/bogeun/abo/AboApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bogeun.abo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
