32327365096cf2be86b8815daa7a9919614f9baa
idiot2222 <pky2892@gmail.com>
2021-01-30 00:17:32 +0900

    add UserController + Test

---
+++ src/main/java/me/bogeun/abo/controller/UserController.java
@@ -0,0 +1,13 @@
+package me.bogeun.abo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/join")
+    public String getJoin() {
+        return "user/join";
+    }
+}
